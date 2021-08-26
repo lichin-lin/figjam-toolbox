4b66c6281bf3e1cd9ace26b357d61043e73997f7
lichin-lin <vic20087cjimlin@gmail.com>
2021-08-26 22:46:45 +0800

    feat: init

---
+++ code.js
@@ -0,0 +1,121 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+figma.showUI(__html__, { width: 300, height: 200 });
+const PREV_TEXT = `🗳 vote(s): `;
+const USER_DATA_ENDPOINT = "user_data";
+figma.loadFontAsync({ family: "Inter", style: "Medium" });
+figma.clientStorage.getAsync(USER_DATA_ENDPOINT).then((data) => {
+    if (!data) {
+        figma.clientStorage.setAsync(USER_DATA_ENDPOINT, []);
+        return;
+    }
+});
+figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    if (msg.type === "create-counter") {
+        const shape = figma.createShapeWithText();
+        shape.shapeType = "ROUNDED_RECTANGLE";
+        shape.name = "body";
+        shape.resize(400, 400);
+        const counterText = figma.createText();
+        counterText.characters = `${PREV_TEXT}0`;
+        counterText.fontSize = 36;
+        counterText.name = "text";
+        counterText.x = shape.x;
+        counterText.y = shape.y - counterText.height * 1.5;
+        const _group = figma.group([shape, counterText], figma.currentPage);
+        console.log("group ID", _group.id);
+        // store in clientStorage
+        figma.clientStorage.getAsync(USER_DATA_ENDPOINT).then((data) => {
+            const _data = [...data, _group.id];
+            figma.clientStorage.setAsync(USER_DATA_ENDPOINT, _data);
+        });
+    }
+    else if (msg.type === "remove-counters") {
+        figma.clientStorage.getAsync(USER_DATA_ENDPOINT).then((data) => __awaiter(this, void 0, void 0, function* () {
+            data === null || data === void 0 ? void 0 : data.forEach((itemID) => {
+                const element = figma.currentPage.findChild((e) => e.id === itemID);
+                if (element) {
+                    element.remove();
+                }
+            });
+            figma.clientStorage.setAsync(USER_DATA_ENDPOINT, []);
+        }));
+    }
+    else if (msg.type === "find-counter") {
+        figma.clientStorage.getAsync(USER_DATA_ENDPOINT).then((data) => __awaiter(this, void 0, void 0, function* () {
+            if (!data)
+                return;
+            let winnerIndex = -1;
+            const countList = data === null || data === void 0 ? void 0 : data.map((itemID) => {
+                const element = figma.currentPage.findChild((e) => e.id === itemID);
+                if (element) {
+                    const text = element.findChild((e) => e.name.includes("text"));
+                    const count = parseInt(text.name.slice(4), 10);
+                    return count || 0;
+                }
+                return 0;
+            });
+            winnerIndex = indexOflargest(countList);
+            if (winnerIndex !== -1) {
+                const toNode = [
+                    figma.currentPage.findChild((c) => c.id === data[winnerIndex]),
+                ];
+                figma.currentPage.selection = toNode;
+                figma.viewport.scrollAndZoomIntoView(toNode);
+            }
+        }));
+    }
+});
+setInterval(() => {
+    const allStampElements = figma.currentPage.findAll((e) => e.type === "STAMP");
+    const allStampPos = allStampElements.map((element) => getElementPos(element));
+    // check allStampPos if inside the counter:
+    if (allStampPos) {
+        figma.clientStorage.getAsync(USER_DATA_ENDPOINT).then((data) => __awaiter(this, void 0, void 0, function* () {
+            data === null || data === void 0 ? void 0 : data.forEach((itemID) => {
+                const element = figma.currentPage.findChild((e) => e.id === itemID);
+                // get group, find the sticky inside the group
+                if (element) {
+                    const area = element.findChild((e) => e.name === "body");
+                    const text = element.findChild((e) => e.name.includes("text"));
+                    if (area && text) {
+                        const areaPos = getElementPos(area);
+                        const count = calcStampInArea(areaPos, allStampPos);
+                        text.characters = `${PREV_TEXT} ${count}`;
+                        text.name = `text${count}`;
+                    }
+                }
+            });
+        }));
+    }
+}, 1000);
+const getElementPos = (element) => {
+    return {
+        x0: element.absoluteTransform[0][2],
+        x1: element.absoluteTransform[0][2] + element.width,
+        y0: element.absoluteTransform[1][2],
+        y1: element.absoluteTransform[1][2] + element.height,
+    };
+};
+const calcStampInArea = (area, stamps) => {
+    let result = 0;
+    stamps.forEach((stamp) => {
+        if (stamp.x1 > area.x0 &&
+            stamp.x0 < area.x1 &&
+            stamp.y1 > area.y0 &&
+            stamp.y0 < area.y1) {
+            result += 1;
+        }
+    });
+    return result;
+};
+function indexOflargest(a) {
+    return a.indexOf(Math.max.apply(Math, a));
+}
